1b3b213bfd6db489899973f20da45ec6a322d0d3
FaithKwamboka <okongofaith3@gmail.com>
2022-05-18 02:14:40 +0300

    Updating views

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+from pickle import GET
 from flask import abort, render_template
 from flask import render_template,request,redirect,url_for
 from flask_login import login_required,current_user
@@ -60,19 +61,44 @@ def review(id):
 
 @main.route('/new_review/<int:bikes_id>', methods = ['GET', 'POST'])
 @login_required
-def new_comment(bikes_id):
+def new_review(bikes_id):
     bikes = Bikes.query.filter_by(bikes_id = bikes_id).first()
     form = ReviewForm()
 
     if form.validate_on_submit():
-        comment = form.review.data
+        review = form.review.data
 
-        new_comment = Reviews(review=review,user_id=current_user.id, bikes_id=bikes_id)
+        new_review = Reviews(review=review,user_id=current_user.id, bikes_id=bikes_id,bikes=bikes)
 
 
-        new_comment.save_comment()
+        new_review.save_review()
 
 
         return redirect(url_for('main.index'))
     title='New Bike'
     return render_template('new_review.html',title=title,review_form = form,bikes_id=bikes_id)
+
+main.route('/new_bike<int:id>', methods=['GET','POST'])
+@login_required
+def new_bike():
+    
+    title = 'Bike Hire'
+    
+    form = BikeForm()
+    if form.validate_on_submit():
+        category = form.category.data
+        bike_pic= form.image
+        bike_obj = Bikes(category=category,bike_pic=bike_pic)
+        bike_obj.save()
+        return redirect(url_for('main.catalog'))
+    return render_template('new_bike.html', bike_form=form)
+
+@main.route('/categories/<category>')
+def category(category):
+    '''
+    function to return the pitches by category
+    '''
+    category = Bikes.get_bikes(category) #get_bikes defined in the bikes route
+    # print(category)
+    title = f'{category}'
+    return render_template('catalogue.html',title = title, category = category)
