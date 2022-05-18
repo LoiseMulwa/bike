2604a0eb80843f735229e310b833b61634517931
Abdi Ali <alikune33@gmail.com>
2022-05-18 12:43:36 +0300

    creating login and signup page

--- app/__init__.py
+++ app/__init__.py
@@ -1,40 +1,37 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
-from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 from flask_login import LoginManager
-from flask_uploads import UploadSet, configure_uploads, IMAGES
-from flask_mail import Mail
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
-bootstrap = Bootstrap()
 db = SQLAlchemy()
-photos = UploadSet('photos', IMAGES)
-mail = Mail()
-def create_app(config_name):
+DB_NAME = "database.db"
+
+
+def create_app():
     app = Flask(__name__)
-    # Creating the app configurations
-    app.config.from_object(config_options[config_name])
-    # configure UploadSet
-    # configure_uploads(app, photos)
-    # Initializing flask extensions
-    bootstrap.init_app(app)
+    app.config['SECRET_KEY'] = 'jsohghosh ghjeimshahga'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    from .models import User
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-    mail.init_app(app)
-    # Registering the blueprint
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
 
 
-
-
-
-
-
-
-
+def create_database(app):
+    if not path.exists('website/'+DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
